fa4d61a4fe99dd6063054af3a34135f56b0fd363
MARGYYY1231 <138099922+MARGYYY1231@users.noreply.github.com>
2023-09-05 22:12:21 +1200

    Moved files to root

---
+++ myths.js
@@ -0,0 +1,24 @@
+let i = 0;
+function read() {
+  if (!i) {
+    document.getElementById('more').style
+      .display = 'inline';
+
+    document.getElementById('dots').style
+      .display = 'inline';
+
+    document.getElementById('read').innerHTML = 'Read less';
+
+    i = 1;
+  } else {
+    document.getElementById('more').style
+      .display = 'none';
+
+    document.getElementById('dots').style
+      .display = 'inline';
+
+    document.getElementById('read').innerHTML = 'Read More';
+
+    i = 0;
+  }
+}
